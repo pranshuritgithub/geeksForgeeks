827b7d4b2d9134c44afe389de1cb24067cd9cee5
pranshuritgithub <prashantjais004@gmail.com>
2025-04-28 22:58:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.kthMissing(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int num = 1;
+        int i = 0;
+        
+        while(i < n && k > 0)
+        {
+            if(arr[i] == num)
+            {
+                i++;
+            }
+            else{
+                k--;
+            }
+            
+            num++;
+        }
+        
+        while(k > 0)
+        {
+            k--;
+            num++;
+        }
+        
+        
+        return num-1;
+    }
+}
