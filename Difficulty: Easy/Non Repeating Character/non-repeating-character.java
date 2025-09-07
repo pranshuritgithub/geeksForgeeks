430d8017332f1f2b3a0c6f0be063fdaded8850dc
pranshuritgithub <prashantjais004@gmail.com>
2025-09-07 19:31:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -1,36 +1,6 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            String st = sc.next();
-
-            char ans = new Solution().nonRepeatingChar(st);
-
-            if (ans != '$')
-                System.out.println(ans);
-            else
-                System.out.println(-1);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    static char nonRepeatingChar(String s) {
-        
+    public char nonRepeatingChar(String s) {
+        // code here
         int n = s.length();
         int freq[] = new int[256];
         
@@ -38,6 +8,7 @@ class Solution {
         {
             freq[s.charAt(i)]++;
         }
+        
         for(int i=0;i<n;i++)
         {
             if(freq[s.charAt(i)] == 1)
