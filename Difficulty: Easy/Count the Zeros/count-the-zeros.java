f88cebddc37eee3e3cc267aa8f870600dcb0e9fe
pranshuritgithub <prashantjais004@gmail.com>
2025-11-28 00:19:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count the Zeros/count-the-zeros.java
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.java
@@ -2,13 +2,13 @@
 
 class Solution {
     int countZeroes(int[] arr) {
-        
+        // code here
         int n = arr.length;
         int count = 0;
         
         for(int i=0;i<n;i++)
         {
-            if(arr[i] == 0)
+            if(arr[i]%2 == 0)
             {
                 count++;
             }
