1468926af5f68afd583caaf6b1ba6552b05814b6
pranshuritgithub <prashantjais004@gmail.com>
2026-01-04 19:10:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -1,61 +1,31 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            // Read first array
-            String input = br.readLine();
-            String[] inputArray = input.split(" ");
-            int[] arr = Arrays.stream(inputArray).mapToInt(Integer::parseInt).toArray();
-
-            Solution ob = new Solution();
-            int res = ob.longestConsecutive(arr);
-
-            System.out.println(res);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
 class Solution {
 
     // Function to return length of longest subsequence of consecutive integers.
     public int longestConsecutive(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
+        if (arr.length == 0)
+            return 0;
 
-        // Add all elements to the set
-        for (int num : arr) {
-            set.add(num);
-        }
+        Arrays.sort(arr);
 
-        int longestStreak = 0;
+        int longest = 1;
+        int current = 1;
 
-        for (int num : arr) {
-            // Check if it is the start of a sequence
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                // Count consecutive numbers
-                while (set.contains(currentNum + 1)) {
-                    currentNum++;
-                    currentStreak++;
-                }
-
-                // Update max length
-                longestStreak = Math.max(longestStreak, currentStreak);
+        for (int i = 1; i < arr.length; i++) {
+            // Ignore duplicates
+            if (arr[i] == arr[i - 1]) {
+                continue;
+            }
+            // Consecutive element
+            else if (arr[i] == arr[i - 1] + 1) {
+                current++;
+            }
+            // Break in sequence
+            else {
+                longest = Math.max(longest, current);
+                current = 1;
             }
         }
 
-        return longestStreak;
+        return Math.max(longest, current);
     }
 }
