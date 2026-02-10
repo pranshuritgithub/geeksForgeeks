b1a9788f60adc9122f1dc1f1e97868a7be4d49c4
pranshuritgithub <prashantjais004@gmail.com>
2026-02-10 22:35:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.java
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -1,59 +1,23 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // Number of test cases
-        for (int g = 0; g < t; g++) {
-            String[] str =
-                (br.readLine()).trim().split(" "); // Reading input as a string array
-            int arr[] = new int[str.length]; // Creating integer array from the input
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-
-            // Getting the result from the Solution class
-            List<Integer> result = new Solution().findDuplicates(arr);
-            Collections.sort(result);
-            // Printing the result in the required format
-            if (result.isEmpty()) {
-                System.out.println("[]");
-            } else {
-                for (int i = 0; i < result.size(); i++) {
-                    if (i != 0) System.out.print(" ");
-                    System.out.print(result.get(i));
-                }
-                System.out.println();
-            }
-            System.out.println("~");
+class Solution {
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        // code here
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int num : arr)
+        {
+            map.put(num, map.getOrDefault(num,0)+1);
         }
+        for(int num : arr)
+        {
+            if(map.get(num) > 1)
+            {
+                list.add(num);
+                map.put(num,0);  // mark as already added
+            }
+        }
+       
+        return list;
     }
 }
-
-// } Driver Code Ends
-
-// Solution class to find duplicates
-class Solution {
-    public List<Integer> findDuplicates(int[] arr) {
-        Map<Integer, Integer> freqMap =
-            new HashMap<>(); // HashMap to store the frequency of elements
-
-        // Count the frequency of each element in the array
-        for (int num : arr) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-        }
-
-        List<Integer> result = new ArrayList<>(); // List to store the result
-
-        // Check for elements that appear more than once
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            if (entry.getValue() > 1) {
-                result.add(entry.getKey());
-            }
-        }
-
-        return result; // Return the list of duplicates or empty list if none
-    }
-}.
