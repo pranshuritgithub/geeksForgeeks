b90d211f6914c0b1314122fa5336b81c5da61d4f
pranshuritgithub <prashantjais004@gmail.com>
2026-02-12 16:40:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,54 +1,23 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
-    static boolean isBalanced(String s) {
+    public boolean isBalanced(String s) {
         // code here
-         Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else if (c == ')' || c == '}' || c == ']') {
-                if (stack.isEmpty()) return false;
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray())
+        {
+            if(ch == '(' || ch == '[' || ch == '{')
+            {
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                
                 char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
-                    return false;
-                }
+                
+                if(ch == ')' && top != '(') return false;
+                if(ch == ']' && top != '[') return false;
+                if(ch == '}' && top != '{') return false;
             }
         }
-
         return stack.isEmpty();
     }
 }
