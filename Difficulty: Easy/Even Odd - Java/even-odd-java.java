27f3ab6ca1d564bf1c7cb48cd0fa5893e7c2a7a2
pranshuritgithub <prashantjais004@gmail.com>
2026-03-17 18:57:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd - Java/even-odd-java.java
@@ -0,0 +1,18 @@
+class Geeks {
+    static void evenOdd(int a, int b) {
+        
+        if (a % 2 == 0) {
+            System.out.println(a);
+        }
+        if (b % 2 == 0) {
+            System.out.println(b);
+        }
+        
+        if (a % 2 != 0) {
+            System.out.println(a);
+        }
+        if (b % 2 != 0) {
+            System.out.println(b);
+        }
+    }
+}
